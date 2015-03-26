9cfd0220e597a7af2bf9c17687ad7b81825613af
alexispetalas <alexispetalas@gmail.com>
2015-03-25 23:55:03 -0300

    init first server

---
+++ app.js
@@ -0,0 +1,35 @@
+var fs = require('fs');
+var express = require('express');
+var mongoose = require('mongoose');
+var config = require('config');
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+var connect = function () {
+  var options = { server: { socketOptions: { keepAlive: 1 } } };
+  mongoose.connect(config.db, options);
+};
+connect();
+
+mongoose.connection.on('error', console.log);
+mongoose.connection.on('disconnected', connect);
+
+fs.readdirSync(__dirname + '/app/models').forEach(function (file) {
+  if (~file.indexOf('.js')) require(__dirname + '/app/models/' + file);
+});
+
+// Bootstrap application settings
+require('./config/express')(app, passport);
+
+// Bootstrap routes
+require('./config/routes')(app, passport);
+
+app.listen(port);
+console.log('Express app started on port ' + port);
+
+/**
+ * Expose
+ */
+
+module.exports = app;
