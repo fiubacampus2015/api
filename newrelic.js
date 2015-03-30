fbbb63bfac61efc5c50eadfb0da79f7631135c03
apetalas <alexispetalas@gmail.com>
2015-03-29 22:57:17 -0300

    test

--- newrelic.js
+++ newrelic.js
@@ -12,7 +12,7 @@ exports.config = {
   /**
    * Your New Relic license key.
    */
-  license_key : 'dd5b2444a2b527d4bcbf693527176847bc70fc04',
+  license_key : '',
   logging : {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
