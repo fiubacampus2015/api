644f6c7707704d2454327fb69e2da6dfec06e77b
tapiajimena <jimena.soledad.tapia@gmail.com>
2015-04-25 23:57:21 -0300

    formato de mail y pagina de confirmacion de registro https://app.asana.com/0/29427082947133/31190359240630

--- config/mailer.js
+++ config/mailer.js
@@ -1,7 +1,13 @@
 var mailer   = require("mailer"),
 config = require("./config");
+var fs = require('fs');
+
+var mailTemplate = fs.readFileSync('./app/views/confirm_mail.html');
+
 
 exports.sendConfirmation = function(to, link, callback) {
+
+  var currentTemplate = mailTemplate.replace('$link',link);
 
   mailer.send(
   { host:           "smtp.mandrillapp.com"
@@ -9,7 +15,7 @@ exports.sendConfirmation = function(to, link, callback) {
   , to:             to
   , from:           "noreply@campus.fi.uba.com.ar"
   , subject:        "campus 2015, finalizar registro"
-  , body:           "Bienvenido al campus fiuba 2015, para finalizar confirma tu registro: " + link
+  , body:           mailTemplate
   , authentication: "login"
   , username:       "app35427088@heroku.com"
   , password:       "qwbuII_mpMyq_pKV8AtEgw"
