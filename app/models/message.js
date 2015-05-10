8788c5b6b87aeae6ba2dd7b3c076a429f83c71bf
tapiajimena <jimena.soledad.tapia@gmail.com>
2015-05-10 18:46:45 -0300

    se agregan datos al modelo del mensaje https://app.asana.com/0/29427082947133/30369862616179

--- app/models/message.js
+++ app/models/message.js
@@ -4,8 +4,11 @@ var crypto = require('crypto');
 var Schema = mongoose.Schema;
 
 var MessageSchema = new Schema({
-  content: { type: String, default: '' },
-  user: { type: Schema.Types.ObjectId, ref:'User' }
+	typeOf: {type:String},
+	date: {type:Date},
+  	content: { type: String, default: '' },
+  	userTo: { type: Schema.Types.ObjectId, ref:'User' },
+  	userFrom: { type: Schema.Types.ObjectId, ref:'User' }
 });
 
 mongoose.model('Message', MessageSchema);
